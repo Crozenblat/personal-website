4533875fbc2779342d2a01553baab3b6c4fe9d44
Corey Rozenblat <coreyrozenblat@gmail.com>
2021-07-25 20:13:18 -0400

    Edited file structure for hosting

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + "/public"));
 app.set("port", (process.env.PORT || 3000));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/pages/index.html"));
+    res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 app.listen(app.get("port"), function(){
