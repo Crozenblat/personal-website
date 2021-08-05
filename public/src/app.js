aa29cd65ae9b3bff9dcbb10a020bb26f4f7f506e
Corey Rozenblat <coreyrozenblat@gmail.com>
2021-08-04 22:32:41 -0400

    Adjusted banner responsiveness and banner down button animation

--- public/src/app.js
+++ public/src/app.js
@@ -16,22 +16,24 @@ const throttle = (fn, wait) => {
     let bannerVideo =  document.querySelector(".banner_video");
     bannerVideo.play();
 
-
-    let banner = document.querySelector(".banner");
-    banner.style.height = `${window.innerHeight}px`;
-
-    window.addEventListener("resize", () => {
-        banner.style.height = `${window.innerHeight}px`;
-        console.log(window.innerWidth);
-    });
-
     let sections = Array.from(document.querySelectorAll("div[class^=section--]"));
+    let bannerDownBtnWrppr = document.querySelector(".banner_down-btn-wrapper");
+    let bannerDownBtn = document.querySelector(".banner_down-btn");
+
+
+    setTimeout(() => {
+        bannerDownBtnWrppr.classList.remove("contents-invisible");
+    }, 1500);
+
+    setTimeout(() => {
+        bannerDownBtn.classList.add("pulse");
+    }, 2500);
+
 
     window.addEventListener("scroll", () => {
         throttle(
         sections.forEach(section => {
-            console.log(section.getBoundingClientRect().top + document.body.scrollTop)
-            if((section.getBoundingClientRect().top + document.body.scrollTop) < 500){
+            if((section.getBoundingClientRect().top + document.body.scrollTop) < 200){
                 section.classList.remove("contents-invisible");
             };
         }), 100);
